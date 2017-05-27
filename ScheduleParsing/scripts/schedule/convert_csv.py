e2e88cbca394502643fc18c5ee99ba4679d65270
fidelcoria <fxc140330@utdallas.edu>
2017-05-26 20:29:21 -0500

    factored out Assgn; need to use new {}.format style

--- ScheduleParsing/scripts/schedule/convert_csv.py
+++ ScheduleParsing/scripts/schedule/convert_csv.py
@@ -1,53 +1,25 @@
 
-#INDECIES FOR THE SPLIT CSV LINE
-DATE = 0
-TYPE = 1
-NAME = 2
-HHLD = 3
-LSSN = 4
-SECT = 5
-
-class Assignment:
-
-    def __init__(self, csv):
-        
-        record = csv.split(',')
-        
-        self.date = record[DATE]
-        self.type = record[TYPE]
-        self.name = record[NAME]
-        self.householder = record[HHLD]
-        self.lesson = record[LSSN]
-        self.section = record[SECT]
+from schedule.Assignment import *
         
         
-prepInsert = """INSERT INTO assignment (date_assgn, assignee, lesson, `section`, assgn_type)
+prepInsertNoHHold = """INSERT INTO assignment (date_assgn, assignee, lesson, `section`, assgn_type)
     SELECT '%s', id_person, %s, '%s', %s
     FROM person
     WHERE full_name = '%s';"""
 
+    
+prepInsertWithHHold = 
+    """
+    INSERT INTO assignment (date_assgn, assignee, householder, lesson, `section`, assgn_type)
+        SELECT '%s', publisher.id_person, hholder.id_person, %s, '%s', %s
+        FROM person as publisher 
+        JOIN person as hholder
+        WHERE publisher.full_name = '%s' and hholder.full_name = '%s'
+    """
+
 prepUpdate = """UPDATE assignment
     SET householder = (SELECT id_person FROM person WHERE full_name = '%s')
     WHERE date_assgn = '%s' and assignee = (SELECT id_person FROM person WHERE full_name = '%s');"""
-
-
-def makeCSV(name, info):
-
-    namedelim = name.find(' ') # find first space (not correct when there is a middle name)
-
-    gender = info[0]
-    fname = name[ : namedelim]
-    lname = name[namedelim+1 : ]
-    active = info[1]
-
-    reading = info[2]
-    initial = info[3]
-    returnv = info[4]
-    bible   = info[5]
-
-    csv = ','.join([gender, lname, fname, active,
-                    reading, initial, returnv, bible])
-    return csv
 
 
 def to_sql(year, month):
@@ -57,21 +29,25 @@ def to_sql(year, month):
     # open csv schedule for parsing (this path only works when script is called from main.py)
     csvfilename = '../csv/%d-%d.csv' % (year, month)
     with open(csvfilename, 'r') as assignments:
-
+        assgn = Assignment() # object used for holding an assignment
+        
         for assignment in assignments:
             
-            assgn = Assignment( assignment.strip() )
+            assgn.setFromCSV( assignment.strip() )
             
             if assgn.lesson == '':
                 assgn.lesson = 'null'
+                
+            # hhold will be automatically set to null by ommision in prepInsertNoHHold
             
-            stmt = prepInsert % (assgn.date, assgn.lesson, assgn.section, assgn.type, assgn.name)
-
-            sqlStatements.append(stmt)
+            if assgn.householder == '':
+                stmt = prepInsertNoHHold % (assgn.date, assgn.lesson, assgn.section, assgn.type, assgn.name)
+                sqlStatements.append(stmt)
             
-            if assgn.householder != '':
-                upStmt = prepUpdate % (assgn.householder, assgn.date, assgn.name)
-                sqlStatements.append(upStmt)
+            else:
+                stmt = prepInsertWithHHold % (assgn.householder, assgn.date, assgn.name)
+                sqlStatements.append(stmt)
+            
 
     sqlfilename = '../sql/%d-%d.sql' % (year, month)
     with open(sqlfilename, 'w') as assgn_sql_f:
